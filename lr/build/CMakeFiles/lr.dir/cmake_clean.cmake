3364ea3d0d9f2840461860c35ffa5de8988a5cbd
LoqmanSamani <samaniloqman91@gmail.com>
2025-04-19 00:48:34 +0200

    !!!

---
+++ lr/build/CMakeFiles/lr.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lr.dir/main.cpp.o"
+  "CMakeFiles/lr.dir/main.cpp.o.d"
+  "lr"
+  "lr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
